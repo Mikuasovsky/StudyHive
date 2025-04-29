0f4f734bbf4f526f93fa4dbff62ed309e2af7832
Mikuasovsky <120600479+Mikuasovsky@users.noreply.github.com>
2025-04-29 21:39:45 +0100

    Entrega Intermedia

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 document.getElementById('searchForm').addEventListener('submit', function(e){
-    e.preventDefault();
-  
-    const disciplina = document.getElementById('disciplina').value.toLowerCase();
-    const nivel = document.getElementById('nivel').value;
-    const modalidade = document.getElementById('modalidade').value;
-  
-    const resultados = getExplicadores().filter(explicador => {
-      return (
-        (disciplina === '' || explicador.disciplina.toLowerCase().includes(disciplina)) &&
-        (nivel === '' || explicador.nivel === nivel) &&
-        (modalidade === '' || explicador.modalidade === modalidade)
-      );
-    });
-  
-    renderExplicadores(resultados);
+  e.preventDefault();
+
+  const disciplina = document.getElementById('disciplina').value.toLowerCase();
+  const nivel = document.getElementById('nivel').value;
+  const modalidade = document.getElementById('modalidade').value;
+
+  const resultados = getExplicadores().filter(explicador => {
+    return (
+      (disciplina === '' || explicador.disciplina.toLowerCase().includes(disciplina)) &&
+      (nivel === '' || explicador.nivel === nivel) &&
+      (modalidade === '' || explicador.modalidade === modalidade)
+    );
   });
+
+  renderExplicadores(resultados);
+});
+
+// preencher o dropdown das disciplinas
+function preencherDisciplinasDropdown() {
+  const disciplinas = JSON.parse(localStorage.getItem('disciplinas')) || ['Matemática', 'Física', 'Inglês','História','Ciencias','Economia II','Português'];
+  const disciplinaSelect = document.getElementById('disciplina');
+
+  disciplinas.forEach(d => {
+    const option = document.createElement('option');
+    option.value = d;
+    option.textContent = d;
+    disciplinaSelect.appendChild(option);
+  });
+}
+
+//dropdown assim que carregar
+preencherDisciplinasDropdown();
--- js/model.js
+++ js/model.js
@@ -16,6 +16,42 @@ const explicadores = [
       modalidade: "Online",
       preco: 25,
       descricao: "Doutorado em Física."
+    },
+    {
+      id: 3,
+      nome: "Pedro Salgado",
+      disciplina: "História",
+      nivel: "Basico",
+      modalidade: "Online",
+      preco: 30,
+      descricao: "Aprende a história comigo."
+    },
+    {
+      id: 4,
+      nome: "Américo Tomás",
+      disciplina: "Ciencias",
+      nivel: "Basico",
+      modalidade: "Presencial",
+      preco: 15,
+      descricao: "Por que as estrelas não fazem miau? Porque astronomia."
+    },
+    {
+      id: 5,
+      nome: "Jorge Nuno Pinto da Costa",
+      disciplina: "Economia II",
+      nivel: "Universitario",
+      modalidade: "Presencial",
+      preco: 45,
+      descricao: "Aprende a contar os pontos."
+    },
+    {
+      id: 6,
+      nome: "Sónia Malhão",
+      disciplina: "Português",
+      nivel: "Secundario",
+      modalidade: "Presencial",
+      preco: 20,
+      descricao: "Adoro Eça de queirós."
     }
   ];
   
